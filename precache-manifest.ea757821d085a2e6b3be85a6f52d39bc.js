4e319950fc0ad0da186aa104ea8332583f568c7b
Taemin Kim <100333592+Taemin0411@users.noreply.github.com>
2023-01-04 21:37:15 +0900

    first commit

---
+++ precache-manifest.ea757821d085a2e6b3be85a6f52d39bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "002703299000a65ccf77c7a514ffc9a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c14cef7f710d8c14552",
+    "url": "/static/css/main.55bfdf87.chunk.css"
+  },
+  {
+    "revision": "058b82cef07657adfa45",
+    "url": "/static/js/2.f06508c7.chunk.js"
+  },
+  {
+    "revision": "34981cc0942697370fc9d635062a9d94",
+    "url": "/static/js/2.f06508c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c14cef7f710d8c14552",
+    "url": "/static/js/main.978af618.chunk.js"
+  },
+  {
+    "revision": "bd00b011b89de116e745",
+    "url": "/static/js/runtime-main.85d7b3f1.js"
+  }
+]);
